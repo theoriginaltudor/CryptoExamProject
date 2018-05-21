9aa6362500d82bc8622a50ddce96c77dfb180fc4
tyrajunior <theoriginaltudor@gmail.com>
2018-05-21 09:51:48 +0200

    Added hibrid encryption. It works even if the client sends error at one point.

--- Server-Client Testing/ClientTest.py
+++ Server-Client Testing/ClientTest.py
@@ -1,19 +1,33 @@
 import socket               # Import socket module
+import nacl.secret
+import nacl.utils
+from nacl.public import SealedBox, PublicKey
+from nacl.encoding import HexEncoder
 
+key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                 # Reserve a port for your service.
 
 s.connect((host, port))
-f = open('C:\\Users\\BurimSyla\\PycharmProjects\\CryptoExamProject\\Server-Client Testing\\file.txt','rb')
-print = 'Sending...'
-l = f.read(1024)
+
+pk = s.recv(64)
+loaded_public_key = PublicKey(pk, encoder=HexEncoder)
+sealing_box = SealedBox(loaded_public_key)
+print(key)
+message = sealing_box.encrypt(key)
+s.send(message)
+box = nacl.secret.SecretBox(key)
+
+f = open('file.txt','rb')
+print('Sending...')
+l = box.encrypt(f.read(1024))
 while (l):
-    print = 'Sending...'
+    print('Sending...')
     s.send(l)
-    l = f.read(1024)
+    l = box.encrypt(f.read(1024))
 f.close()
-print = "Done Sending"
+print("Done Sending")
 s.shutdown(socket.SHUT_WR)
-print = s.recv(1024)
-s.close     # Close the socket when done
+print(s.recv(1024))
+s.close()     # Close the socket when done
--- Server-Client Testing/ServerTest.py
+++ Server-Client Testing/ServerTest.py
@@ -1,21 +1,37 @@
-import socket               # Import socket module
+import socket  # Import socket module
+import nacl.secret
+from nacl.encoding import HexEncoder
+from nacl.public import PrivateKey, SealedBox
+
+sk = PrivateKey.generate()
+pk = sk.public_key
+print(pk)
+pkencoded = pk.encode(HexEncoder)
+print(pkencoded)
+unsealing_box = SealedBox(sk)
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
 port = 12345                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 f = open('torecv.txt','wb')
-s.listen(5)                 # Now wait for client connection.
+s.listen(2)                 # Now wait for client connection.
 while True:
     c, addr = s.accept()     # Establish connection with client.
-    print = 'Got connection from', addr
-    print = "Receiving..."
-    l = c.recv(1024)
+    print("Got connection from", addr)
+    c.send(pkencoded)
+    message = c.recv(1024)
+    plain_text = unsealing_box.decrypt(message)
+    print("Symmetric key: ", plain_text)
+    box = nacl.secret.SecretBox(plain_text)
+
+    print("Receiving...")
+    l = box.decrypt(c.recv(1024))
     while (l):
-        print = "Receiving..."
+        print("Receiving...")
         f.write(l)
-        l = c.recv(1024)
+        l = box.decrypt(c.recv(1024))
     f.close()
-    print = "Done Receiving"
-    c.send('Thank you for connecting')
+    print("Done Receiving")
+    c.send(b"Thank you for connection")
     c.close()                # Close the connection
