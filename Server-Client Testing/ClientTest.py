99c37085e42f4f8b36acd2bf007054a81fa197e1
BurimSyla <bimi.syla@hotmail.com>
2018-06-03 19:17:58 +0200

    comments added

--- Server-Client Testing/ClientTest.py
+++ Server-Client Testing/ClientTest.py
@@ -5,26 +5,32 @@ from nacl.public import Box, PublicKey, PrivateKey
 from nacl.encoding import HexEncoder
 
 sk = PrivateKey.generate()
+print("Client's secret key : ", sk)
 pk = sk.public_key
+print("Client's public key: ", pk)
 pkencoded = pk.encode(HexEncoder)
 key = nacl.utils.random(nacl.secret.SecretBox.KEY_SIZE)
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+host = socket.gethostname()  # Get local machine name
 port = 12345                 # Reserve a port for your service.
 
 s.connect((host, port))
 
 pk_external = s.recv(64)
+print("Public key received from server: ", pk_external)
 s.send(pkencoded)
 loaded_public_key = PublicKey(pk_external, encoder=HexEncoder)
 boxAsymmetric = Box(sk, loaded_public_key)
-message = boxAsymmetric.encrypt(key)
-s.send(message)
-box = nacl.secret.SecretBox(key)
+print("This box contains client secret key and Server's public key : ", boxAsymmetric)
 
-f = open('file.txt','rb')
+message = boxAsymmetric.encrypt(key)  # Encrypt secret key of Client
+s.send(message)                       # Sending secret key encrypted
+box = nacl.secret.SecretBox(key)      # creating a secret box for the message
+
+f = open('file.txt', 'rb')
 print('Sending...')
-l = box.encrypt(f.read(1024))
+l = box.encrypt(f.read(1024))  # Encryption of message
+print("This is the encrypted message: ", l)
 while (l):
     print('Sending...')
     s.send(l)
--- Server-Client Testing/ServerTest.py
+++ Server-Client Testing/ServerTest.py
@@ -4,33 +4,42 @@ from nacl.encoding import HexEncoder
 from nacl.public import PrivateKey, Box, PublicKey
 
 sk = PrivateKey.generate()
+print("This is servers' secret key: ", sk)
 pk = sk.public_key
+print("This is public key which will be sent to Client: ", pk)
 pkencoded = pk.encode(HexEncoder)
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
+host = socket.gethostname()  # Get local machine name
 port = 12345                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
-f = open('torecv.txt','wb')
+f = open('torecv.txt', 'wb')
 s.listen(1)                 # Now wait for client connection.
 while True:
     c, addr = s.accept()     # Establish connection with client.
     print("Got connection from", addr)
     c.send(pkencoded)
+
     pk_external = c.recv(1024)
+    print("This is public key received from Client: ", pk_external)
     loaded_public_key = PublicKey(pk_external, encoder=HexEncoder)
+
     boxAsymmetric = Box(sk, loaded_public_key)
+    print("This box contains server secret key and Client's public key : ", boxAsymmetric)
     message = c.recv(1024)
+    print("This is the encrypted message received: ", message)
     plain_text = boxAsymmetric.decrypt(message)
+
     box = nacl.secret.SecretBox(plain_text)
 
     print("Receiving...")
     l = box.decrypt(c.recv(1024))
+    print("This is the decrypted message: ", l)
     while (l):
         print("Receiving...")
         f.write(l)
         l = box.decrypt(c.recv(1024))
-    f.close()
-    print("Done Receiving")
+        f.close()
+        print("Done Receiving")
     c.send(b"Thank you for connection")
     c.close()                # Close the connection
